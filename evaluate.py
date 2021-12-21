20fa1ff50d1dcb4a7b3799071fa78038e52db804
BruceWang <haoranwang@tju.edu.cn>
2021-12-21 10:57:24 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -39,13 +39,13 @@ parser.add_argument('--fold5', action='store_false', help='Choose to evaluate on
 # parser = argparse.ArgumentParser(description='CVSE evaluate')
 # parser.add_argument('--data_path', default='./Bottom_up_atten_feature/data', help='path to dataset ')
 # parser.add_argument('--data_name', default='f30k_precomp',   help='{coco,f30k}_precomp')
-# parser.add_argument('--concept_name', default='data/coco_annotations/Concept_annotations/category_concepts.json',
+# parser.add_argument('--concept_name', default='data/coco_to_f30k_annotations/Concept_annotations/category_concepts.json',
 #                         help='load the input concrete words of concepts') 
 # parser.add_argument('--model_path', default='./runs/coco/CVSE_COCO/model_best.pth.tar',help='Path to load the model.')
 # parser.add_argument('--vocab_path', default='./vocab/', help='Path to saved vocabulary json files.')
 # parser.add_argument('--data_name_vocab', default='coco_precomp',   help='{coco,f30k}_precomp')
 # parser.add_argument('--transfer_test', action='store_false', help='Whether to perform cross-dataset testing.')
-# parser.add_argument('--concept_path', default='data/coco_to_f30k_annotations/Concept_annotations_coco_to_f30k/',  
+# parser.add_argument('--concept_path', default='data/coco_to_f30k_annotations/Concept_annotations/',  
 #                         help='path to load the concept data') 
 
 
